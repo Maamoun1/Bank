fe887140ac412277fe415d4abf6af0cdad2e3b3b
Mamoun ibrahim <Mamoun_mady@yahoo.com>
2026-02-15 18:48:45 +0200

    Add Basic Shield (HTTPS + CORS)

--- BankApi/Program.cs
+++ BankApi/Program.cs
@@ -6,11 +6,11 @@ using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+//add CORS
 builder.Services.AddCors(options =>
 {
 
@@ -68,6 +68,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
+//Use CORS
 app.UseCors("BankApiCoresPloicy");
 
 app.UseAuthorization();
